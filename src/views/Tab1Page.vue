433d899be5efc9454ab119fff297bc647eb54a97
Kenny Brawner <kjbrawner22@gmail.com>
2022-04-26 22:38:54 -0500

    can get current geolocation

--- src/views/Tab1Page.vue
+++ src/views/Tab1Page.vue
@@ -8,22 +8,44 @@
     <ion-content :fullscreen="true">
       <ion-header collapse="condense">
         <ion-toolbar>
-          <ion-title size="large">Tab 1</ion-title>
+          <ion-title size="large">Geolocation Demo</ion-title>
         </ion-toolbar>
       </ion-header>
-    
-      <ExploreContainer name="Tab 1 page" />
+      <ion-button v-on:click="printCurrentPosition" >Get Current Position</ion-button>
+      <ion-item>
+        <ion-label>Latitude: {{ coordinates.coords.latitude }}</ion-label>
+      </ion-item>
+      <ion-item>
+        <ion-label>Longitude: {{ coordinates.coords.longitude }}</ion-label>
+      </ion-item>
     </ion-content>
   </ion-page>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
-import ExploreContainer from '@/components/ExploreContainer.vue';
+import { IonButton, IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonItem, IonLabel } from '@ionic/vue';
+import { Geolocation } from '@capacitor/geolocation';
 
 export default  defineComponent({
   name: 'Tab1Page',
-  components: { ExploreContainer, IonHeader, IonToolbar, IonTitle, IonContent, IonPage }
+  components: { IonButton, IonHeader, IonToolbar, IonTitle, IonContent, IonPage, IonItem, IonLabel },
+  data() {
+    return {
+      coordinates: {
+        coords: {
+          latitude: 0,
+          longitude: 0
+        }
+      }
+    }
+  },
+  methods: {
+    async printCurrentPosition(event: any) {
+      const coordinates = await Geolocation.getCurrentPosition();
+
+      this.coordinates = coordinates
+    }
+  }
 });
 </script>
